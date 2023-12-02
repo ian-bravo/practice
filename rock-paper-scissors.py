224d34ceedac3c783e89bb5c175e79c37d023f24
ian-bravo <bravo.ian@gmail.com>
2023-12-01 17:01:14 -0800

    refactor

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -2,7 +2,7 @@ import random
 
 
 def get_choice():
-  player_choice = input("Enter a choice (rock, paper, scissors: ")
+  player_choice = input("Enter a choice (rock, paper, scissors): ")
   options = ["rock", "paper", "scissors"]
   computer_choice = random.choice(options)
 
@@ -10,24 +10,24 @@ def get_choice():
   return choices
 
 def check_win(player, computer):
-  print (f"You chose {player} and computer chose {computer}")
+  print (f"You choose {player} and computer chose {computer}")
   if player == computer:
     return ("It's a tie!")
   elif player == "rock":
     if computer == "scissors":
       return ("Rock beats scissors, you win!")
     else:
-      return ("paper beats rock, you lose ):")
+      return ("Paper beats rock, you lose ):")
   elif player == "paper":
     if computer == "rock":
-      return ("paper beats rock, you win!")
+      return ("Paper beats rock, you win!")
     else:
-      return ("scissors beat paper, you lose ):")
+      return ("Scissors beat paper, you lose ):")
   elif player == "scissors":
     if computer == "paper":
-      return ("scissors beats paper, you win!")
+      return ("Scissors beats paper, you win!")
     else:
-      return ("rock beats scissors, you lose ):")
+      return ("Rock beats scissors, you lose ):")
 
 choices = get_choice()
 result = check_win(choices["player"], choices["computer"])
